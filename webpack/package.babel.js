202e4463478f8dfe69f21904c1ee55bf25f0cd5a
KingPar <exile1280@hotmail.com>
2024-02-15 21:48:43 -0500

    Add files via upload

---
+++ webpack/package.babel.js
@@ -0,0 +1,29 @@
+import * as fs from 'fs';
+
+const tryRead = filename => {
+  try {
+    return JSON.parse(fs.readFileSync(filename).toString());
+  } catch(e) {
+    return null;
+  }
+};
+
+const pack = {
+  ...tryRead("package.json"),
+  scripts: {
+    start: "node server.js",
+  },
+};
+delete pack.devDependencies;
+
+fs.writeFileSync("dist/package.json", JSON.stringify(pack, null, 4));
+
+
+const configs = {
+  port: 3939,
+  ...tryRead("configs.json"),
+  ...tryRead("dist/configs.json"),
+};
+
+fs.writeFileSync("dist/configs.json", JSON.stringify(configs, null, 4));
+fs.copyFileSync("package-lock.json", "dist/package-lock.json");
